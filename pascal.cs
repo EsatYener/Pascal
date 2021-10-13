25ed2c2f4627b2f6750171fc887117ee1bec278b
EsatYener <92437764+EsatYener@users.noreply.github.com>
2021-10-13 19:34:27 +0300

    Add files via upload

---
+++ pascal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+public class Pascal
+{
+    public static void Main()
+    {
+        int row, c = 1, i, j, k;
+        while (true)
+        {
+            Console.Write("Enter number of rows: ");
+            string kosul=Console.ReadLine();
+            while (!(int.TryParse(kosul, out row) && row > 0))
+                {
+                    Console.Write("Sıfırdan küçük veya sıfır giremezseniz.");
+                    Console.Write("Yeniden sayı giriniz:");
+                    kosul = Console.ReadLine();
+                }
+            
+
+            for (i = 0; i < row; i++)
+            {
+                for (j = 1; j <= row - i; j++)
+                    Console.Write("  ");
+                for (k = 0; k <= i; k++)
+                {
+                    if (i == 0 || k == 0)
+                        c = 1;
+                    else
+                        c = c * (i - k + 1) / k;
+                    Console.Write("{0}   ", c);
+                }
+                Console.Write("\n");
+            }
+
+        }
+    }
+}
